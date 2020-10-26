e4cdb1d84e88305ece64fbc83b5e7468f85834f4
Diego Abanto <dabanto21@gmail.com>
2020-10-26 00:30:14 -0500

    Como mostrar html en las vistas

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -38,7 +38,9 @@ return 'Saludos ' . $nombre;
 return 'Saludos ' . $nombre;
 }); */
 
-Route::get('contactame', function () {
+// Rutas con nombre
+
+/* Route::get('contactame', function () {
     return 'Seccion de contactos';
 })->name('contactos');
 
@@ -48,4 +50,40 @@ Route::get('/', function () {
     echo "<a href='" . route('contactos') . "'>Contactos 3</a><br>";
     echo "<a href='" . route('contactos') . "'>Contactos 4</a><br>";
     echo "<a href='" . route('contactos') . "'>Contactos 5</a><br>";
-});
+}); */
+
+// Llamar una vista desde una ruta
+
+/* Route::get('/', function () {
+    return view('home');
+}); */
+
+// Pasar parametros desde una ruta a una vista
+
+/* Route::get('/', function () {
+    $nombre = "Diego";
+    // return view('home')->with('nombre', $nombre);
+    
+    // Otra forma es a traves de un arreglo
+    // return view('home')->with(['nombre' => $nombre]);
+
+    // Otra forma es atraves de un arreglo y sin usar with
+    // return view('home', ['nombre' => $nombre]);
+
+    // O tambien usando la funcion "compact"
+    // siempre y cuando tenga el mismo nombre de la variable
+    return view('home', compact('nombre')); // ['nombre' => $nombre]
+}); */
+
+// Otra forma de llamar a la vista "home"
+/* Route::view('/', 'home'); */
+
+// Se le puede pasar un arreglo de la siguiente manera
+// Bastante usado en paginas que no requieren pasar mucha información
+/* Route::view('/', 'home', ['nombre' => 'Diego']); */
+
+// Se le puede asignar un nombre
+Route::view('/', 'home')->name('home');
+Route::view('/about', 'about')->name('about');
+Route::view('/portfolio', 'portfolio')->name('portfolio');
+Route::view('/contact', 'contact')->name('contact');
