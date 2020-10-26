28ecef6bc5528bbcc93e8f102af5b21134986858
Diego Abanto <dabanto21@gmail.com>
2020-10-25 23:42:40 -0500

    Rutas, Parametros obligatorios, Parametros opcionales

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -9,8 +9,31 @@
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+ */
 
-Route::get('/', function () {
-    return view('welcome');
+/* Route::get('/', function () {
+return view('welcome');
+}); */
+
+// Rutas de Prueba
+
+/* Route::get('/', function () {
+return 'Hola desde la pagina de inicio';
+});
+
+Route::get('/contacto', function () {
+return 'Hola desde la pagina de contacto';
+}); */
+
+// Usar parametros en las rutas
+// Parametros obligatorios
+
+/* Route::get('saludo/{nombre}', function ($nombre) {
+    return 'Saludos ' . $nombre;
+}); */
+
+// Parametros opcionales
+
+Route::get('saludo/{nombre?}', function ($nombre = 'Invitado') {
+    return 'Saludos ' . $nombre;
 });
