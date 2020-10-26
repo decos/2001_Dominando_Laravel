a7dc00a86bf89d9149749b3cd740c23e1b02ca36
Diego Abanto <dabanto21@gmail.com>
2020-10-25 23:55:02 -0500

    Rutas con nombre

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -29,11 +29,23 @@ return 'Hola desde la pagina de contacto';
 // Parametros obligatorios
 
 /* Route::get('saludo/{nombre}', function ($nombre) {
-    return 'Saludos ' . $nombre;
+return 'Saludos ' . $nombre;
 }); */
 
 // Parametros opcionales
 
-Route::get('saludo/{nombre?}', function ($nombre = 'Invitado') {
-    return 'Saludos ' . $nombre;
+/* Route::get('saludo/{nombre?}', function ($nombre = 'Invitado') {
+return 'Saludos ' . $nombre;
+}); */
+
+Route::get('contactame', function () {
+    return 'Seccion de contactos';
+})->name('contactos');
+
+Route::get('/', function () {
+    echo "<a href='" . route('contactos') . "'>Contactos 1</a><br>";
+    echo "<a href='" . route('contactos') . "'>Contactos 2</a><br>";
+    echo "<a href='" . route('contactos') . "'>Contactos 3</a><br>";
+    echo "<a href='" . route('contactos') . "'>Contactos 4</a><br>";
+    echo "<a href='" . route('contactos') . "'>Contactos 5</a><br>";
 });
