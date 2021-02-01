9873fa7e107de24292ab301fce9d37754ed70477
Diego Abanto <dabanto21@gmail.com>
2021-02-01 04:14:52 -0500

    Diseño con Bootstrap 4 - parte 4, añadir estilos a la vista crear nuevo proyecto

--- laravel/resources/views/projects/_form.blade.php
+++ laravel/resources/views/projects/_form.blade.php
@@ -1,18 +1,59 @@
 @csrf
 
-<label>
-    Título del proyecto <br>
-    <input type="text" name="title" value="{{ old('title', $project->title) }}">
+<label for="title">
+    Título del proyecto
 </label>
-<br>
-<label>
-    URL del proyecto <br>
-    <input type="text" name="url" value="{{ old('url', $project->url) }}">
+
+<div class="form-group">
+    <input
+        id="title"
+        class="
+            form-control
+            bg-light
+            shadow-sm
+            @if ($errors->has('title')) is-invalid @else border-0 @endif
+        "
+        type="text"
+        name="title"
+        value="{{ old('title', $project->title) }}">
+</div>
+
+<label for="url">
+    URL del proyecto
 </label>
-<br>
-<label>
-    Descripción del proyecto <br>
-    <textarea name="description">{{ old('description', $project->description) }}</textarea>
+
+<div class="form-group">
+    <input
+        id="url"
+        class="
+            form-control
+            bg-light
+            shadow-sm
+            @if ($errors->has('url')) is-invalid @else border-0 @endif
+        "
+        type="text"
+        name="url"
+        value="{{ old('url', $project->url) }}">
+</div>
+
+<label for="description">
+    Descripción del proyecto
 </label>
-<br>
-<button>{{ $btnText }}</button>
+
+<div class="form-group">
+    <textarea
+        id="description"
+        class="
+            form-control
+            bg-light
+            shadow-sm
+            @if ($errors->has('description')) is-invalid @else border-0 @endif
+        "
+        name="description">
+        {{ old('description', $project->description) }}
+    </textarea>
+</div>
+
+<button class="btn btn-primary btn-lg btn-block">
+    {{ $btnText }}
+</button>
